51b92c6587e560ebf021c407e7d6b886571f268c
Aalums <59070194@it.kmitl.ac.th>
2018-11-06 21:10:52 +0700

    หน้า Category : เพิ่มรูป จัดตำแหน่ง และคลิก

--- app/src/main/java/kmitl/it/recipe/recipe/CategoryFragment.java
+++ app/src/main/java/kmitl/it/recipe/recipe/CategoryFragment.java
@@ -9,7 +9,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -24,5 +26,20 @@ public class CategoryFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        clickImg();
+
+    }
+
+    void clickImg (){
+        ImageButton cat_boil = getView().findViewById(R.id.cat_img_catBoil);
+        cat_boil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(
+                        getActivity(),"Click Image", Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
     }
 }
