cebc9533425640a7d0af7c53d1cb6ad0207a64db
preawphuak <59070126@it.kmitl.ac.th>
2018-11-11 00:24:07 +0700

    ADD ChooseMenu Slide Update Color(opacity)

---
+++ app/src/main/java/kmitl/it/recipe/recipe/ChooseMenu/ChooseMenuFragment.java
@@ -0,0 +1,87 @@
+package kmitl.it.recipe.recipe.ChooseMenu;
+
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import kmitl.it.recipe.recipe.R;
+
+public class ChooseMenuFragment extends  Fragment {
+
+    private ViewPager viewpager;
+    private LinearLayout liner;
+    private ChooseSlideAdapter myadapter;
+
+//    private ArrayList<Favorite> _recipe = new ArrayList<>();
+//
+//    final ListView _recipeList = getView().findViewById(R.id.favorite_list);
+//    final ChooseRecipeAdapter _recipeAdapter = new ChooseRecipeAdapter(getActivity(), R.layout.fragment_favorite_item, _recipe);
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_choose_menu, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+//        showListView();
+        controlPath();
+    }
+
+
+
+    private void showViewPager(ViewPager.OnPageChangeListener _menuListener ){
+        viewpager = getView().findViewById(R.id.choose_view_pager);
+        viewpager = getView().findViewById(R.id.choose_view_pager);
+        viewpager =  getView().findViewById(R.id.choose_view_pager);
+        liner = getView().findViewById(R.id.choose_dot);
+
+
+        myadapter = new ChooseSlideAdapter(getActivity());
+        viewpager.setAdapter(myadapter);
+
+        viewpager.addOnPageChangeListener(_menuListener);
+
+    }
+
+    private void  controlPath() {
+
+        ViewPager.OnPageChangeListener _menuListener = new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+            @Override
+            public void onPageSelected(int position) {
+
+               // mCurrentPage = position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+            }
+
+        };
+        showViewPager(_menuListener);
+    }
+
+//    private void showListView() {
+//
+//        ListView _menuList = getView().findViewById(R.id.menu_list);
+//        _menuList.setAdapter(_recipeAdapter);
+//
+//    }
+}
---
+++ app/src/main/java/kmitl/it/recipe/recipe/ChooseMenu/ChooseSlideAdapter.java
@@ -0,0 +1,99 @@
+package kmitl.it.recipe.recipe.ChooseMenu;
+
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import kmitl.it.recipe.recipe.R;
+
+
+public class ChooseSlideAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater inflater;
+
+    public ChooseSlideAdapter(Context context){
+        this.context=context;
+    }
+
+    //Array
+    public int[] list_images={
+
+            0,
+            R.drawable.cat_boiled,
+            R.drawable.cat_boiled,
+            R.drawable.cat_boiled
+    };
+
+    public String[] list_title={
+
+            "แกง",
+            "แกงส้ม",
+            "แกงส้ม",
+            "แกงส้ม"
+    };
+
+    public String[] list_description={
+
+            "เลื่อน >",
+            "แกงเขียวหวานนั่นแสนอร่อย",
+            "แกงเขียวหวานนั่นแสนอร่อย",
+            "แกงเขียวหวานนั่นแสนอร่อย"
+    };
+
+
+    @Override
+    public int getCount() {
+        return list_title.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object obj) {
+        return view == obj;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        inflater = (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.item_choose_menu,container,false);
+
+        LinearLayout linearLayout = view.findViewById(R.id.choosei_slide);
+        ImageView img = view.findViewById(R.id.choosei_img);
+        TextView txt1 =  view.findViewById(R.id.choosei_title);
+        TextView txt2 =  view.findViewById(R.id.choosei_desc);
+
+        if(position == 0) {
+            txt1.setText(list_title[position]);
+            txt2.setText(list_description[position]);
+            linearLayout.setBackgroundColor(Color.rgb(252, 214, 44));
+            img.setImageResource( list_images[position]);
+            img.setScaleType(ImageView.ScaleType.FIT_CENTER);
+
+        }
+        else{
+            txt1.setText(list_title[position]);
+            txt2.setText(list_description[position]);
+            img.setImageResource( list_images[position]);
+            img.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        }
+
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout)object);
+    }
+}
