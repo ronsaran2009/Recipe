20aa81d804164229384f8b593f387d208a40d848
Ron <ronsaran2009@gmail.com>
2018-11-13 10:26:29 +0700

    Change Nav Bar when open right to left && change Nav Bar Menu Item name

--- app/src/main/java/kmitl/it/recipe/recipe/Activity/MainActivity.java
+++ app/src/main/java/kmitl/it/recipe/recipe/Activity/MainActivity.java
@@ -98,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
                 int id = item.getItemId();
 
 
-                if (id == R.id.myprofile){
+                if (id == R.id.nav_menu_mymenu){
                     if (_auth.getCurrentUser() != null){
                         Toast.makeText(MainActivity.this, "ALREADY_LOG_IN", Toast.LENGTH_SHORT).show();
                         Log.d("NAV_MENU", "ALREADY_LOG_IN");
@@ -114,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                     _drawMain.closeDrawers();
                 }
-                else if (id == R.id.nav_menu_category){
+                else if (id == R.id.nav_menu_addmenu){
                     Toast.makeText(MainActivity.this,"CATEGORY",Toast.LENGTH_SHORT).show();
                     getSupportFragmentManager()
                             .beginTransaction()
