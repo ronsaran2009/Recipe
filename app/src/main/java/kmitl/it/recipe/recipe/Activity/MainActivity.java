b102051bc17bd48488366730efc528c0dab47578
Aalums <59070194@it.kmitl.ac.th>
2018-11-13 21:04:24 +0700

    Add : Category Tabs

--- app/src/main/java/kmitl/it/recipe/recipe/Activity/MainActivity.java
+++ app/src/main/java/kmitl/it/recipe/recipe/Activity/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +21,8 @@ import kmitl.it.recipe.recipe.CategoryFragment;
 import kmitl.it.recipe.recipe.HomeFragment;
 import kmitl.it.recipe.recipe.LoginFragment;
 import kmitl.it.recipe.recipe.R;
+import kmitl.it.recipe.recipe.Tab1Fragment;
+import kmitl.it.recipe.recipe.TabAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void fadeInOut(Bundle savedInstanceState){
+    private void fadeInOut(Bundle savedInstanceState) {
         if (COUNT) {
             COUNT = false;
             new Handler().postDelayed(new Runnable() {
@@ -53,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }, WELCOME_TIMEOUT);
             Log.d("Main", "fade | count : " + COUNT);
-        }else {
+        } else {
             if (savedInstanceState == null) {
                 callNavigationBar();
                 checkSelectNavigation();
@@ -62,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void callNavigationBar(){
+    private void callNavigationBar() {
         _auth = FirebaseAuth.getInstance();
         _drawMain = findViewById(R.id.drawMain);
         _abdt = new ActionBarDrawerToggle(this, _drawMain, R.string.open, R.string.close);
@@ -71,12 +75,11 @@ public class MainActivity extends AppCompatActivity {
         _drawMain.addDrawerListener(_abdt);
         _abdt.syncState();
 
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
 
-    private void goToHome(){
+    private void goToHome() {
         getSupportFragmentManager()
                 .beginTransaction()
                 .setCustomAnimations(R.anim.fade_out, R.anim.fade_in)
@@ -86,10 +89,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void checkSelectNavigation(){
+    private TabAdapter tabAdapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    private void checkSelectNavigation() {
         NavigationView nav_view = findViewById(R.id.nav_view);
-
-
 
         nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
 
@@ -98,12 +103,11 @@ public class MainActivity extends AppCompatActivity {
                 int id = item.getItemId();
 
 
-                if (id == R.id.nav_menu_mymenu){
-                    if (_auth.getCurrentUser() != null){
+                if (id == R.id.nav_menu_mymenu) {
+                    if (_auth.getCurrentUser() != null) {
                         Toast.makeText(MainActivity.this, "ALREADY_LOG_IN", Toast.LENGTH_SHORT).show();
                         Log.d("NAV_MENU", "ALREADY_LOG_IN");
-                    }
-                    else {
+                    } else {
                         Toast.makeText(MainActivity.this, "MYPROFILE", Toast.LENGTH_SHORT).show();
                         getSupportFragmentManager()
                                 .beginTransaction()
@@ -113,14 +117,22 @@ public class MainActivity extends AppCompatActivity {
                         Log.d("NAV_MENU", "GOTO PROFILE");
                     }
                     _drawMain.closeDrawers();
-                }
-                else if (id == R.id.nav_menu_addmenu){
-                    Toast.makeText(MainActivity.this,"CATEGORY",Toast.LENGTH_SHORT).show();
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .addToBackStack(null)
-                            .replace(R.id.main_view, new CategoryFragment())
-                            .commit();
+                } else if (id == R.id.nav_menu_addmenu) {
+                    Toast.makeText(MainActivity.this, "CATEGORY", Toast.LENGTH_SHORT).show();
+
+                    //Category Tabs
+                    setContentView(R.layout.category_main);
+                    viewPager = findViewById(R.id.viewPager);
+                    tabLayout = findViewById(R.id.tabLayout);
+                    tabAdapter = new TabAdapter(getSupportFragmentManager());
+                    tabAdapter.addFragment(new Tab1Fragment(), " ต้ม - แกง ");
+                    tabAdapter.addFragment(new Tab1Fragment(), " ผัด - ทอด ");
+                    tabAdapter.addFragment(new Tab1Fragment(), " อบ - ตุ๋น ");
+                    tabAdapter.addFragment(new Tab1Fragment(), " ปิ้ง - ย่าง ");
+                    tabAdapter.addFragment(new Tab1Fragment(), " อาหารจานเดียว");
+                    viewPager.setAdapter(tabAdapter);
+                    tabLayout.setupWithViewPager(viewPager);
+
                     Log.d("NAV_MENU", "GOTO CATEGORY");
                     _drawMain.closeDrawers();
                 }
@@ -134,16 +146,15 @@ public class MainActivity extends AppCompatActivity {
 //                    Log.d("NAV_MENU", "GOTO EDIT_PROFILE");
 //                    _drawMain.closeDrawers();
 //                }
-                else if (id == R.id.nav_menu_singout){
+                else if (id == R.id.nav_menu_singout) {
                     Log.d("NAV_MENU", "elseif");
-                    if (_auth.getCurrentUser() != null){
-                        Toast.makeText(MainActivity.this,"ออกจากระบบเรียบร้อยแล้ว",Toast.LENGTH_SHORT).show();
+                    if (_auth.getCurrentUser() != null) {
+                        Toast.makeText(MainActivity.this, "ออกจากระบบเรียบร้อยแล้ว", Toast.LENGTH_SHORT).show();
                         Log.d("NAV_MENU", "SING OUT COMPLETE");
                         _auth.signOut();
                         TextView _profile = findViewById(R.id.nav_head_text);
                         _profile.setText("Guest");
-                    }
-                    else {
+                    } else {
                         Toast.makeText(MainActivity.this, "ท่านไม่ได้อยู่ในระบบ", Toast.LENGTH_SHORT).show();
                         Log.d("NAV_MENU", "SING OUT BUT NO CURRENT USER");
                     }
@@ -156,7 +167,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(_abdt.onOptionsItemSelected(item))
+        if (_abdt.onOptionsItemSelected(item))
             return true;
 
         return super.onOptionsItemSelected(item);
