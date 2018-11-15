2a94a06e1bf0d44ccf10bf4a53baeb7328d82743
preawphuak <59070126@it.kmitl.ac.th>
2018-11-15 20:46:20 +0700

    update my_menu (Database/Log/layout/add image) | item_show_fragment

--- app/src/main/java/kmitl/it/recipe/recipe/model/Mymenu.java
+++ app/src/main/java/kmitl/it/recipe/recipe/model/Mymenu.java
@@ -8,10 +8,48 @@ public class Mymenu {
 
     public Mymenu() { }
 
+    public Mymenu(String menu) {
+        this.menu = menu;
+    }
+
     public Mymenu(String menu, String type, String date, String writer) {
         this.menu = menu;
         this.type = type;
         this.date = date;
         this.writer = writer;
+
     }
+    public String getMenu() {
+        return menu;
+    }
+
+    public void setMenu(String menu) {
+        this.menu = menu;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+
 }
