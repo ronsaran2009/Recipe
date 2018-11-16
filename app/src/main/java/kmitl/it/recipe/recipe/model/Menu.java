03f850d9300030bf67f20eed8e9d9c7e0d098919
Aalums <59070194@it.kmitl.ac.th>
2018-11-17 02:59:10 +0700

    Show profileMenu | code show profileUser

--- app/src/main/java/kmitl/it/recipe/recipe/model/Menu.java
+++ app/src/main/java/kmitl/it/recipe/recipe/model/Menu.java
@@ -12,6 +12,7 @@ public class Menu {
     private String category;
     private String time;
     private String profileMenu;
+    private String profileUser;
 
     String writer;
     String link;
@@ -99,6 +100,14 @@ public class Menu {
 
     public void setProfileMenu(String profileMenu) {
         this.profileMenu = profileMenu;
+    }
+
+    public String getProfileUser() {
+        return profileUser;
+    }
+
+    public void setProfileUser(String profileMenu) {
+        this.profileUser = profileUser;
     }
 
     public String getWriter() { return writer; }
